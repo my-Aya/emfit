da4c5d7e57c366fe8b5ab168d4bb3cc22a5b19a2
Lucas Silva Simões <simoes.lucas.silva@gmail.com>
2022-06-27 16:16:39 -0300

    feat: correct bugs and skip figure prompt

--- batchRunEMfit.m
+++ batchRunEMfit.m
@@ -1,42 +1,42 @@
 function batchRunEMfit(modelClassToFit,Data,resultsDir,varargin)
-% 
+%
 % batchRunEMfit(modelClassToFit,pathToData,resultsDir,'key1','val1','key2','val2');
-% 
+%
 % Performs batch EM inference by first fitting a set of models from each model
 % class, plotting the inferred parameters, performing iBIC model comparison,
 % generating surrogate data and performing some visual comparisons of the true
-% and surrogate data. 
+% and surrogate data.
 %
-% MODELCLASSTOFIT determines which model sets are fitted: 
-% 
-% 'mBasicRescorlaWagner';			% basic Rescorla-Wagner example 
-% 'mAffectiveGoNogo';				% Guitart et al. 2012 
-% 'mProbabilisticReward';			% Huys et al., 2013 
-% 'mTwostep';							% Daw et al., 2011 
-% 'mEffortCollins';	 				% Gold et al., 2013 
-% 'mPruning'; 						   % Lally et al., 2017 
+% MODELCLASSTOFIT determines which model sets are fitted:
+%
+% 'mBasicRescorlaWagner';			% basic Rescorla-Wagner example
+% 'mAffectiveGoNogo';				% Guitart et al. 2012
+% 'mProbabilisticReward';			% Huys et al., 2013
+% 'mTwostep';							% Daw et al., 2011
+% 'mEffortCollins';	 				% Gold et al., 2013
+% 'mPruning'; 						   % Lally et al., 2017
 % 'mEffortDDM';                  % Berwian et al., 2020
 %
 % DATA (optional) contains the data.  See the dataformat.txt files in the model
 % folders for instructions on how the data contained in DATA should be formatted
 % for fitting. For demo purposes, a correct dataset is generated if no data is
-% provided. 
-% 
+% provided.
+%
 % RESULTSDIR (optional) is a path to a directory containing the results. If it
-% is not provided then fitResults in the current working directory is used. 
-% 
-% In addition, the following key-value pairs are accepted: 
-% 
+% is not provided then fitResults in the current working directory is used.
+%
+% In addition, the following key-value pairs are accepted:
+%
 % MODELSTOFIT (optional) is a vector with indices of specific models to fit, as
-% per the modelList definition in each model folder mXXXX. 
-% 
+% per the modelList definition in each model folder mXXXX.
+%
 % CHECKGRADIENTS (optional) can be set to 1 to check the gradients of the
-% likelihood functions using finite differences. 
-% 
-% MAXIT (optional) limits the maximal number of EM iterations. 
-% 
+% likelihood functions using finite differences.
+%
+% MAXIT (optional) limits the maximal number of EM iterations.
+%
 % Quentin Huys, 2018 qhuys@cantab.net
-% 
+%
 %==============================================================================
 
 if exist('resultsDir')~=1 || isempty(resultsDir)
@@ -45,7 +45,7 @@ end
 warning('off','MATLAB:MKDIR:DirectoryExists');
 mkdir(resultsDir);
 
-% check optional arguments 
+% check optional arguments
 validvarargins = {'modelsToFit','checkgradients','maxit','bsub'};
 if exist('varargin')
 	for k=1:length(validvarargins)
@@ -55,44 +55,45 @@ if exist('varargin')
 end
 
 %------------------------------------------------------------------------------
-% MODEL CLASS - define which type of model to fit 
- 
-modelClass{1} = 'mBasicRescorlaWagner';         % basic example 
-modelClass{2} = 'mAffectiveGoNogo';             % Guitart et al. 2012 
-modelClass{3} = 'mProbabilisticReward';         % Huys et al., 2013 
-modelClass{4} = 'mTwostep';                     % Daw et al., 2011 
-modelClass{5} = 'mEffortCollins';               % Gold et al., 2013 
-modelClass{6} = 'mPruning';                     % Lally et al., 2017 
-modelClass{7} = 'mEffortDDM';                   % Berwian et al., 2020 
+% MODEL CLASS - define which type of model to fit
 
-modelClassToFit = find(cellfun(@(x)strcmp(x,modelClassToFit),modelClass)); 
+modelClass{1} = 'mBasicRescorlaWagner';         % basic example
+modelClass{2} = 'mAffectiveGoNogo';             % Guitart et al. 2012
+modelClass{3} = 'mProbabilisticReward';         % Huys et al., 2013
+modelClass{4} = 'mTwostep';                     % Daw et al., 2011
+modelClass{5} = 'mEffortCollins';               % Gold et al., 2013
+modelClass{6} = 'mPruning';                     % Lally et al., 2017
+modelClass{7} = 'mEffortDDM';                   % Berwian et al., 2020
+modelClass{8} = 'mSelfOtherJudgement';          % Alena 2022 
+
+modelClassToFit = find(cellfun(@(x)strcmp(x,modelClassToFit),modelClass));
 if isempty(modelClassToFit); error('Model class not found');end
 
 %==============================================================================
-% everything below here should just run without alterations 
+% everything below here should just run without alterations
 
 %------------------------------------------------------------------------------
-% get model descriptions 
+% get model descriptions
 emfitpath = fileparts(which('batchRunEMfit'));
-addpath([emfitpath '/lib']); 
-cleanpath(modelClass);									% clean all model paths 
+addpath([emfitpath '/lib']);
+cleanpath(modelClass);									% clean all model paths
 addpath(genpath([emfitpath '/' modelClass{modelClassToFit}]));	% add chosen model path
-models=modelList; 										% get complete model list 
+models=modelList; 										% get complete model list
 if exist('modelsToFit')
-	models = models(modelsToFit); 					% select specific models to fit 
+	models = models(modelsToFit); 					% select specific models to fit
 end
 
 %------------------------------------------------------------------------------
 % generate surrogate data if no data was provided
 if ~exist('Data', 'var') || isempty(Data)
 	fprintf('No data provided so generating example dataset\n');
-	Data=generateExampleDataset(30,resultsDir); 			
+	Data=generateExampleDataset(30,resultsDir);
 end
 
 %------------------------------------------------------------------------------
 % fit models using emfit.m
 if exist('checkgradients')
-	options.checkgradients = checkgradients;		% check gradients of models? 
+	options.checkgradients = checkgradients;		% check gradients of models?
 end
 if exist('maxit')
 	options.maxit = maxit; 								% limit EM iterations
@@ -100,24 +101,24 @@ end
 options.resultsDir = resultsDir; 					% directory with results
 batchModelFit(Data,models,options); 				% perform the fitting
 
-close all; 
+close all;
 
 %------------------------------------------------------------------------------
-% perform model comparison 
+% perform model comparison
 bestmodel = batchModelComparison(Data,models,resultsDir);
 
 %------------------------------------------------------------------------------
-% plot parameters of best model 
-batchParameterPlots(Data,models,resultsDir,bestmodel); 
+% plot parameters of best model
+batchParameterPlots(Data,models,resultsDir,bestmodel);
 
 %------------------------------------------------------------------------------
-% generate surrogate data 
+% generate surrogate data
 options.nSamples=100;
 batchGenerateSurrogateData(Data,models,options);
 
 %------------------------------------------------------------------------------
 % plot surrogate data, compare with real data (specific to model)
-load([resultsDir '/SurrogateData']); 
+load([resultsDir '/SurrogateData']);
 surrogateDataPlots(Data,models,SurrogateData,bestmodel,resultsDir)
 
 fprintf('\n\nAll done!\n\n')
--- lib/myfig.m
+++ lib/myfig.m
@@ -1,13 +1,14 @@
 function [name, file]=myfig(gcf,name,mfile,varargin);
 % [name, figurefile]=myfig(gcf,name,mfile,filelist);
 % take input gcf and set paperpositionmode to auto and all tickmodes to manual
-% name should include directory 
+% name should include directory
 % print an eps file and a fig file
 % additionally copy the m.file used to make the figure there
 % and finally also copy all files in the filelist cell to a .tar.gz in the same
-% place 
+% place
 
-in=input(['do you really want to save the figure as ',name,'.eps? [y for yes, else no]'],'s');
+% in=input(['do you really want to save the figure as ',name,'.eps? [y for yes, else no]'],'s');
+in = 'y'
 
 if length(varargin)==1
 	furtherfiles = varargin{1};
@@ -18,7 +19,7 @@ if strcmp(in,'y')
 	set(gcf,'paperpositionmode','auto')
 	t=get(gcf,'children');
 	for k=1:length(t);
-		try 
+		try
 			set(t(k),'xtickmode','manual','ytickmode','manual','ztickmode','manual');
 		end
 	end
@@ -27,7 +28,7 @@ if strcmp(in,'y')
 	saveas(gcf,strcat(name,'.fig'));
 
 	if exist('mfile'); eval(['!cp ',mfile,' ',name,'.m']);end
-	if exist('furtherfiles'); 
+	if exist('furtherfiles');
 		filelist=[name '.eps ' name '.fig ' file ' '];
 		for k=1:length(furtherfiles)
 			filelist=[filelist ' ' furtherfiles{k}];
