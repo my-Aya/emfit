f7b7201ec5c4231dc0141363faa550a7efef5a9f
Lucas Silva Simões <simoes.lucas.silva@gmail.com>
2022-06-27 16:16:02 -0300

    feat: add new models

---
+++ mSelfOtherJudgement/lld6avbnowords.m
@@ -0,0 +1,84 @@
+function [l,dl,dsurr] = lld6avbnowords(x,D,mu,nui,doprior,options);
+%
+% [l,dl,surrugatedata] = lld6avbnowords(x,D,mu,nui,doprior,options);
+%
+% log likelihood (l) and gradient (dl) of ___
+%
+% Use this within emfit.m to tit RL type models to a group of subjects using
+% EM.
+%
+% Quentin Huys 2021
+
+dodiff=nargout==2;
+np = size(x,1);
+
+rho = exp(x(1));
+alpha = 1./(1+exp(-x(2)));
+selfposbias = x(3:4);
+
+% add Gaussian prior with mean mu and variance nui^-1 if doprior = 1
+[l,dl] = logGaussianPrior(x,mu,nui,doprior);
+
+a = D.a;
+r = D.r;
+wordval = D.wordval;
+avatval = D.avatval;
+av = D.avatid;
+
+Q = zeros(2,12);
+dqdr = zeros(2,12);
+dqda = zeros(2,12);
+
+if options.generatesurrogatedata==1
+	a = zeros(size(a));
+	dodiff=0;
+end
+
+for t=1:length(a);
+	if ~isnan(a(t));
+
+		bl = 1+(t>48);
+		wv = (-wordval(t)+3)/2;
+		q0 = Q(:,av(t));
+		q0(1) = q0(1) + selfposbias(bl)*wordval(t);
+
+		l0 = q0-max(q0);
+		l0 = l0 - log(sum(exp(l0)));
+		p = exp(l0);
+
+		if options.generatesurrogatedata==1
+			[a(t),r(t)] = generatera(p,wordval(t),avatval(t));
+		end
+		l = l+l0(a(t));
+
+		if dodiff
+			dl(1) = dl(1) + dqdr(a(t),av(t)) - p'*dqdr(:,av(t));
+			dl(2) = dl(2) + dqda(a(t),av(t)) - p'*dqda(:,av(t));
+			tmp = [wordval(t);0];
+			dl(2+bl)   = dl(2+bl) + tmp(a(t)) - p'*tmp;
+
+			dqdr(a(t),av(t)) = dqdr(a(t),av(t)) + alpha*(rho*r(t) - dqdr(a(t),av(t)));
+			dqdr(3-a(t),av(t)) = dqdr(3-a(t),av(t)) + alpha*(-rho*r(t) - dqdr(3-a(t),av(t)));
+			dqda(a(t),av(t)) = dqda(a(t),av(t)) + alpha*(1-alpha)*(rho*r(t)-Q(a(t),av(t))) + alpha*(-dqda(a(t),av(t)));
+			dqda(3-a(t),av(t)) = dqda(3-a(t),av(t)) + alpha*(1-alpha)*(-rho*r(t)-Q(3-a(t),av(t))) + alpha*(-dqda(3-a(t),av(t)));
+		end
+
+		Q(  a(t),av(t)) = Q(  a(t),av(t))+alpha*( rho*r(t)-Q(  a(t),av(t)));
+		Q(3-a(t),av(t)) = Q(3-a(t),av(t))+alpha*(-rho*r(t)-Q(3-a(t),av(t)));
+
+	else
+		if options.generatesurrogatedata==1
+			a(t) = NaN;
+			r(t) = NaN;
+		end
+	end
+end
+
+l = -l;
+dl = -dl;
+
+if options.generatesurrogatedata==1
+	dsurr.a = a;
+	dsurr.r = r;
+end
+
--- mSelfOtherJudgement/modelList.m
+++ mSelfOtherJudgement/modelList.m
@@ -1,56 +1,97 @@
-function model = modelList; 
-% 
+function model = modelList;
+%
 % A file like this should be contained in each model class folder and list the
-% models to be run, together with some descriptive features. 
-% 
-% GENERAL INFO: 
-% 
+% models to be run, together with some descriptive features.
+%
+% GENERAL INFO:
+%
 % list the models to run here. The models must be defined as likelihood functions in
-% the models folder. They must have the form: 
-% 
+% the models folder. They must have the form:
+%
 %    [l,dl,dsurr] = ll(parameters,dataToFit,PriorMean,PriorInverseCovariance,doPrior,otherOptions)
-% 
+%
 % where otherOptions.generatesurrogatedata is a binary flag defining whether to apply the prior, and
 % doGenerate is a flag defining whether surrogate data (output in asurr) is
-% generated. 
-% 
+% generated.
+%
 % name: names of model likelihood function in folder models
-% npar: number of paramters for each 
+% npar: number of paramters for each
 % parnames: names of parameters for plotting
 % partransform: what to do to the (transformed) parameter estimates to transform them into the
 % parameters
-%  
-% SPECIFIC INFO: 
-% 
-% This contain models for the self/other judgement task. 
-% 
+%
+% SPECIFIC INFO:
+%
+% This contain models for the self/other judgement task.
+%
 % Quentin Huys 2021
 
-i=0; 
+i=0;
 
-i=i+1; 
-model(i).descr = 'simple model with self and other bias towards accepting positive words';
-model(i).name = 'llb';			
+i=i+1;
+model(i).descr = 'simple model (no learning) with self and other bias towards accepting positive words';
+model(i).name = 'llb';
 model(i).npar = 2;
 model(i).parnames = {'bias self','bias other'};
 model(i).parnames_untr = {'b_s','b_o'};
 model(i).partransform = {'@(x)x','@(x)x'};
 
-i=i+1; 
-model(i).descr = 'RW model, restart for each avatar and two self/other bias parameters';
-model(i).name = 'll6avb';			
+i=i+1;
+model(i).descr = 'RW model, restart for each avatar and two self/other bias parameters (shared across words). Words are part of state';
+model(i).name = 'll6avb';
 model(i).npar = 4;
 model(i).parnames = {'\rho','\alpha','bias self','bias other'};
 model(i).parnames_untr = {'log \rho','siginv \alpha','b_s','b_o'};
 model(i).partransform = {'@(x)exp(x)','@(x)1./(1+exp(-x))','@(x)x','@(x)x'};
 
-i=i+1; 
-model(i).descr = 'RW model with double update, restart for each avatar and two self/other bias parameters';
-model(i).name = 'lld6avb';			
+i=i+1;
+model(i).descr = 'RW model with double update (for opposite action, same word), restart for each avatar and two self/other bias parameters (shared across words). Words are part of state';
+model(i).name = 'lld6avb';
 model(i).npar = 4;
 model(i).parnames = {'\rho','\alpha','bias self','bias other'};
 model(i).parnames_untr = {'log \rho','siginv \alpha','b_s','b_o'};
 model(i).partransform = {'@(x)exp(x)','@(x)1./(1+exp(-x))','@(x)x','@(x)x'};
 
-nModls = i; 
+i=i+1;
+model(i).descr = 'RW model with quadruple update (for all 4 action-word combinations), restart for each avatar and two self/other bias parameters (shared across words). Words are part of state';
+model(i).name = 'llq6avb';
+model(i).npar = 4;
+model(i).parnames = {'\rho','\alpha','bias self','bias other'};
+model(i).parnames_untr = {'log \rho','siginv \alpha','b_s','b_o'};
+model(i).partransform = {'@(x)exp(x)','@(x)1./(1+exp(-x))','@(x)x','@(x)x'};
+
+i=i+1;
+model(i).descr = 'RW model, restart for each avatar and two self/other bias parameters (shared across words). No words in state';
+model(i).name = 'll6avbnowords';
+model(i).npar = 4;
+model(i).parnames = {'\rho','\alpha','bias self','bias other'};
+model(i).parnames_untr = {'log \rho','siginv \alpha','b_s','b_o'};
+model(i).partransform = {'@(x)exp(x)','@(x)1./(1+exp(-x))','@(x)x','@(x)x'};
+
+i=i+1;
+model(i).descr = 'RW model with double update (for opposite action), restart for each avatar and two self/other bias parameters (shared across words). No words in state';
+model(i).name = 'lld6avbnowords';
+model(i).npar = 4;
+model(i).parnames = {'\rho','\alpha','bias self','bias other'};
+model(i).parnames_untr = {'log \rho','siginv \alpha','b_s','b_o'};
+model(i).partransform = {'@(x)exp(x)','@(x)1./(1+exp(-x))','@(x)x','@(x)x'};
+
+i=i+1;
+model(i).descr = 'RW model, restart for each avatar and four bias parameters (self/other, pos/neg words). Words are part of state';
+model(i).name = 'll6av4b';
+model(i).npar = 6;
+model(i).parnames = {'\rho','\alpha','bias self neg','bias other neg', 'bias self pos', 'bias other pos'};
+model(i).parnames_untr = {'log \rho','siginv \alpha','b_{s,n}','b_{o,n}','b_{s,p}','b_{o,p}'};
+model(i).partransform = {'@(x)exp(x)','@(x)1./(1+exp(-x))','@(x)x','@(x)x','@(x)x','@(x)x'};
+
+i=i+1;
+model(i).descr = 'RW model with quadruple update, restart for each avatar and four bias parameters (self/other, pos/neg words). Words are part of state';
+model(i).name = 'llq6av4b';
+model(i).npar = 6;
+model(i).parnames = {'\rho','\alpha','bias self neg','bias other neg', 'bias self pos', 'bias other pos'};
+model(i).parnames_untr = {'log \rho','siginv \alpha','b_{s,n}','b_{o,n}','b_{s,p}','b_{o,p}'};
+model(i).partransform = {'@(x)exp(x)','@(x)1./(1+exp(-x))','@(x)x','@(x)x','@(x)x','@(x)x'};
+
+
+nModls = i;
 fprintf('%i models in model list\n',i);
